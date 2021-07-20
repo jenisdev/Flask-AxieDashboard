60ee80b0d2a26c9a793f9a1f599d7042f937ac4e
Son518 <ujin518@outlook.com>
2021-07-20 17:51:42 +0100

    ddd

--- run.py
+++ run.py
@@ -2,9 +2,5 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-import os
-import sys
-basedir = os.path.abspath(os.path.dirname(__file__))
-print(basedir)
-sys.path.insert(0, '/var/www/axie-dashboard')
+
 from app import app, db
