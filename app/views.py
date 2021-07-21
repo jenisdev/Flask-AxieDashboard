f4e853c9bd0e795f4c64548b8b5ecc5f7c7dac1b
Son518 <ujin518@outlook.com>
2021-07-21 07:41:24 +0100

    update

--- app/views.py
+++ app/views.py
@@ -19,6 +19,48 @@ from app.models import User, Scholarship
 from app.forms  import LoginForm, RegisterForm
 from sqlalchemy.sql import func
 
+from datetime import datetime
+
+
+def humanize_ts(timestamp=False):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    now = datetime.now()
+    diff = now - datetime.fromtimestamp(timestamp)
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str(int(second_diff)) + " seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return str(int(second_diff / 60)) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str(int(second_diff / 3600)) + " hours ago"
+    if day_diff == 1:
+        return "Yesterday"
+    if day_diff < 7:
+        return str(day_diff) + " days ago"
+    if day_diff < 31:
+        return str(int(day_diff / 7)) + " weeks ago"
+    if day_diff < 365:
+        return str(int(day_diff / 30)) + " months ago"
+    return str(int(day_diff / 365)) + " years ago"
+
+app.jinja_env.filters['humanize'] = humanize_ts
+
 # provide login manager with load_user callback
 @lm.user_loader
 def load_user(user_id):
@@ -155,12 +197,9 @@ def tracker():
         Scholarship.ClaimedSLP.label('claimed'), \
         Scholarship.ManagerShare.label('manager'), \
         Scholarship.ScholarShare.label('scholar'), \
+        Scholarship.LastClaim.label('lastclaim'), \
         )\
         .all()
-
-    import sys, logging
-    logger = logging.getLogger(__name__)
-    logger.debug("------------------------: ")
-    logger.debug(slpdata)
+    
     return render_template('scholar-tracker.html', slpdata=slpdata, tabledata=tabledata)
 
