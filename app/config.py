97f7e676f88c927df11169e9648024ea99525f37
Son518 <ujin518@outlook.com>
2021-08-11 20:27:39 +0100

    fixing UI bugs

--- app/config.py
+++ app/config.py
@@ -19,6 +19,6 @@ class Config():
     MAIL_DEFAULT_SENDER = 'baidawardipendar@gmail.com'
 
     # This will create a file in <app> FOLDER
-    # SQLALCHEMY_DATABASE_URI = 'mysql://dbmasteruser:)+wH{,O3xEU$DHW0fU)AuTwJNipA+0^T@ls-16a5009e4d9680ee447daa82c8ece035b4bedff2.cvasw40kycww.eu-west-2.rds.amazonaws.com/dbmaster' #'sqlite:///db.sqlite3'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/axie' #'sqlite:///db.sqlite3'
+    SQLALCHEMY_DATABASE_URI = 'mysql://dbmasteruser:)+wH{,O3xEU$DHW0fU)AuTwJNipA+0^T@ls-16a5009e4d9680ee447daa82c8ece035b4bedff2.cvasw40kycww.eu-west-2.rds.amazonaws.com/dbmaster' #'sqlite:///db.sqlite3'
+    # SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/axie' #'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
